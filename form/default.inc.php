749a79c75bfac50c5d34183b1b8ebe48d6b42f68
Brittany Pigott <brittpigott@gmail.com>
2021-11-25 08:44:25 -0500

    initial commit

---
+++ form/default.inc.php
@@ -0,0 +1,133 @@
+<style type="text/css">
+* {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+}
+
+h2 {
+  padding: 16px 0;
+}
+
+h1,
+h2 {
+  font-family: "Playfair Display", serif;
+}
+
+p {
+  padding: 10px 0 15px;
+}
+
+img {
+  height: 300px;
+  width: 450px;
+}
+
+.container {
+  background: white;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 20px;
+  min-height: 100vh;
+}
+
+.top-bar {
+  width: 100%;
+  padding: 8px;
+  background: #14203b;
+  color: #f4f4f4;
+  position: fixed;
+  display: inline-block;
+}
+
+nav {
+  float: right;
+}
+
+.logo {
+  font-family: "Birthstone Bounce", serif;
+  float: left;
+  padding: 10px;
+}
+
+nav ul li {
+  display: inline-block;
+  vertical-align: top;
+}
+
+nav ul li a {
+  display: block;
+  padding: 15px;
+  text-decoration: none;
+  color: #f4f4f4;
+  font-size: 15px;
+  transition: 0.3s;
+}
+
+nav ul li a:hover {
+  color: #326986;
+  transition: 0.3s;
+}
+
+footer {
+  text-align: center;
+  background: #14203b;
+  color: #f4f4f4;
+  padding: 40px 0px;
+}
+
+footer a {
+  color: white;
+  font-size: 18px;
+  margin: 0 3px;
+  transition: 0.3s;
+}
+
+footer a:hover {
+  color: #c5c2c2;
+  transition: 0.3s;
+}
+
+footer p {
+  padding: 5px;
+}
+</style>
+
+
+<header>
+        <section class="top-bar">
+            <h1 class="logo">Brittany Pigott</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="portfolio.html">PORTFOLIO</a></li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                </ul>
+            </nav>
+        </section>
+
+        <section class="top-header">
+            <div class="border">
+                <h1>Contact</h1>
+                <hr>
+            </div>
+        </section>
+    </header>
+
+<div class="container">
+    <h1>Sorry something went wrong with your submission</h1>
+    <p>Please try again</p>
+<div>
+
+<footer>
+    <p class="website">Website By Brittany Pigott</p>
+    <span>FOLLOW ME</span>
+        <a href="https://facebook.com" target="blank"><i class="fab fa-facebook-square"></i></a>
+        <a href="http://instagram.com" target="blank"><i class="fab fa-instagram"></i></a>
+        <a href="http://twitter.com" target="blank"><i class="fab fa-twitter"></i></a>
+</footer>
